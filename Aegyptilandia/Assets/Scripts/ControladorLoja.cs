965be68d4f153efd0442b1907756518f96b49af2
Thiago <thiagolamf@gmail.com>
2016-02-17 21:44:58 -0200

    Adicionado ControladorLoja

---
+++ Aegyptilandia/Assets/Scripts/ControladorLoja.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ControladorLoja : MonoBehaviour {
+
+	public Text teste;
+	// Use this for initialization
+	void Start () {
+		teste.text = "OLA";
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void compraVida (){
+		//int moedas = GameObject.Find("Personagem").GetComponent<ControladorPersonagem>().Coins;
+		//float vida = GameObject.Find ("Personagem").GetComponent<ControladorPersonagem> ().vida;
+		//teste.text = "" + moedas + "vida:" + vida;
+		//Debug.Log (moedas);
+	}
+
+	public void compraVeneno(){
+
+	}
+
+	public void compraAgua(){
+	}
+}
